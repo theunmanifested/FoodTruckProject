df12fe436eaf48d33c1c5dd1c4f5ea9611001b41
theunmanifested <waltersoso80@gmail.com>
2021-01-23 08:11:11 -0600

    Continued building the two classes. So far, cannot properly populate FoodTruck objects as the For Loop data user input gets skippy on the second iteration between questions

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -7,19 +7,28 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
-	private static int NUMID = 2100; // each food truck will have a unique numeric Id
-	private double numRating; // scale from 1.0 (worst) to 10.0 (best)
 	private String truckName;
 	private String foodType;
+	private double numRating; // scale from 1.0 (worst) to 10.0 (best)
+	private static int NUMID = 2100; // each food truck will have a unique numeric Id
 
+	// no-arg constructor
+	public FoodTruck() {
+		
+	}
+	
 	// Constructor does not allow numId to be instantiated by the creation of an
 	// object of this class
-	public FoodTruck(double numRating, String truckName, String foodType) {
-		this.numRating = numRating;
+	public FoodTruck(String truckName, String foodType, double numRating, int NUMID) {
 		this.truckName = truckName;
 		this.foodType = foodType;
-		FoodTruck.NUMID = NUMID++; // assigned automatically and incremented at instantiation
+		this.numRating = numRating;
+		FoodTruck.NUMID = NUMID++;
 	}
+	
+
+	
+	
 
 	public static int getNUMID() {
 		return NUMID;
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -5,37 +5,48 @@ import java.util.Scanner;
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		FoodTruck[] fT= new FoodTruck[4]; // only 5 food truck objects
-		
-		// Prompt User to input the name, food type, and rating for up to five food trucks
+		FoodTruck[] fTarr = new FoodTruck[4]; // only 5 food truck objects
+
+		// Prompt User to input the name, food type, and rating for up to five food
+		// trucks
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Hello There! Thanks for rating up to five food trucks.");
-		System.out.println("For your first food truck review. Also, you can type 'quit' when entering the Food Truck Name to exit. Thanks!");		
-		System.out.print("Please enter the Food Truck Name: ");
-		
+		System.out.println(
+				"For your first food truck review. Also, you can type 'quit' when entering the Food Truck Name to exit. Thanks!");
+		// create a FoodTruck Object per food truck being reviewed
+		FoodTruck fT = new FoodTruck();
 		// restrict to only five food trucks per user
-		for (int i = 0; i < fT.length; i++) {
+		for (int i = 0; i < fTarr.length; i++) {
+
+			// prompt for Food Truck Name
+			System.out.print("Please enter the Food Truck Name: ");
 			// check if user's Truck Name Input was: quit
-			if (kb.nextLine().toUpperCase().equals("QUIT")) {
+			// Food Truck Name
+			String fTName = kb.nextLine();
+			if (fTName.toUpperCase().equals("QUIT")) {
 				System.out.println("You've opted to quit");
 				break;
 			}
-			// enter user's input data onto each FoodTruck object:
-			// Food Truck Name
-			fT[i].setTruckName(kb.nextLine());
+//			kb.nextLine(); // help with console flow
+			// keep enter user's input data onto each FoodTruck object:
+			fT.setTruckName(fTName);
 			// Food Truck Type
 			System.out.print("Please enter the Food Truck's Food Type: ");
-			fT[i].setFoodType(kb.nextLine());
+			String fTType = kb.nextLine();
+			fT.setFoodType(fTType);
 			// Food Truck Rating
-			System.out.print("Using the scale from 1.0 (worse) to 10.0 (best), please rate the Food Truck: ");
-			fT[i].setNumRating(kb.nextDouble());
+			System.out.print("Using the scale from 1.0 (worse) to 10.0 (best), please rate the Food Truck: \n");
+			double rating = kb.nextDouble();
+			fT.setNumRating(rating);
 			// NUMID is assigned automatically from the constructor
-		}
-		
+			fTarr[i] = fT;
+		} // end of For Loop of user input
+
 		// TEST: print out the Food Trucks
-		for (int i = 0; i < fT.length; i++) {
-			fT[i].displayFoodTruck();
-		}		
+		for (int i = 0; i < fTarr.length; i++) {
+			fTarr[i].displayFoodTruck();
+		}
+		kb.close();
 	} // end of main
 
 } // end of class
